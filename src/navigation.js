c23a1def0456118e02ac68452f5a43c9d798df23
Psim <clipclop@gmail.com>
2024-05-27 14:58:07 +0000

    minimal maybe working

--- src/navigation.js
+++ src/navigation.js
@@ -56,35 +56,35 @@ export const headerData = {
     //     },
     //   ],
     // },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
+    // {
+    //   text: 'Landing',
+    //   links: [
+    //     {
+    //       text: 'Lead Generation',
+    //       href: getPermalink('/landing/lead-generation'),
+    //     },
+    //     {
+    //       text: 'Long-form Sales',
+    //       href: getPermalink('/landing/sales'),
+    //     },
+    //     {
+    //       text: 'Click-Through',
+    //       href: getPermalink('/landing/click-through'),
+    //     },
+    //     {
+    //       text: 'Product Details (or Services)',
+    //       href: getPermalink('/landing/product'),
+    //     },
+    //     {
+    //       text: 'Coming Soon or Pre-Launch',
+    //       href: getPermalink('/landing/pre-launch'),
+    //     },
+    //     {
+    //       text: 'Subscription',
+    //       href: getPermalink('/landing/subscription'),
+    //     },
+    //   ],
+    // },
     // {
     //   text: 'Blog',
     //   links: [
@@ -119,56 +119,56 @@ export const headerData = {
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        // { text: 'Enterprise', href: '#' },
-        // { text: 'Customer stories', href: '#' },
-        // { text: 'Pricing', href: '#' },
-        // { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        // { text: 'Partners', href: '#' },
-        // { text: 'Atom', href: '#' },
-        // { text: 'Electron', href: '#' },
-        // { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        // { text: 'Professional Services', href: '#' },
-        // { text: 'Skills', href: '#' },
-        // { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        // { text: 'Blog', href: '#' },
-        // { text: 'Careers', href: '#' },
-        // { text: 'Press', href: '#' },
-        // { text: 'Inclusion', href: '#' },
-        // { text: 'Social Impact', href: '#' },
-        // { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
+  // links: [
+  //   {
+  //     title: 'Product',
+  //     links: [
+  //       { text: 'Features', href: '#' },
+  //       { text: 'Security', href: '#' },
+  //       { text: 'Team', href: '#' },
+  //       // { text: 'Enterprise', href: '#' },
+  //       // { text: 'Customer stories', href: '#' },
+  //       // { text: 'Pricing', href: '#' },
+  //       // { text: 'Resources', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Platform',
+  //     links: [
+  //       { text: 'Developer API', href: '#' },
+  //       // { text: 'Partners', href: '#' },
+  //       // { text: 'Atom', href: '#' },
+  //       // { text: 'Electron', href: '#' },
+  //       // { text: 'AstroWind Desktop', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Support',
+  //     links: [
+  //       { text: 'Docs', href: '#' },
+  //       { text: 'Community Forum', href: '#' },
+  //       // { text: 'Professional Services', href: '#' },
+  //       // { text: 'Skills', href: '#' },
+  //       // { text: 'Status', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Company',
+  //     links: [
+  //       { text: 'About', href: '#' },
+  //       // { text: 'Blog', href: '#' },
+  //       // { text: 'Careers', href: '#' },
+  //       // { text: 'Press', href: '#' },
+  //       // { text: 'Inclusion', href: '#' },
+  //       // { text: 'Social Impact', href: '#' },
+  //       // { text: 'Shop', href: '#' },
+  //     ],
+  //   },
+  // ],
+  // secondaryLinks: [
+  //   { text: 'Terms', href: getPermalink('/terms') },
+  //   { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  // ],
   // socialLinks: [
   //   { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
   //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
